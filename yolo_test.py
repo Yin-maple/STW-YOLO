dee62288bafa62cc6963905b000c589996b6a7ae
Yin Yufeng <1499961892@qq.com>
2025-11-10 14:18:26 +0800

    change

--- yolo_test.py
+++ yolo_test.py
@@ -3,23 +3,17 @@ import sys
 
 if __name__ == '__main__':
     # 消融模型
-    ablition_model = ["yolov8s","yolo11s","YOLOv11s+P2","YOLOv11s+P2+rP5","p2_rp5_new_yolo11s","p2_rp5_yolo11s"][-1]
-    # ablition_model = sys.argv[1]
+    #ablition_model = ["yolov8s","yolo11s","YOLOv11s+P2","YOLOv11s+P2+rP5","p2_rp5_new_yolo11s","p2_rp5_yolo11s"][-1]
+    ablition_model = sys.argv[1]
+    data_type = sys.argv[2]
+    size = sys.argv[3]
     # 加载模型
-    print("model:",ablition_model)
+    print("model:",ablition_model,"data:",data_type)
     model = YOLO('./runs/{}/weights/best.pt'.format(ablition_model))  # build a new model from scratch
     print('========================================================================')
-    res = model.val(data=r"./Lib/contr_worker.yaml", epochs=200, imgsz=640, batch=32, save_json=True)
-    print(res)
-    model.val(data=r"./Lib/mocs_test.yaml", epochs=200, imgsz=640, batch=32, save_json=True,name = ablition_model+"/"+"mocs_test" )
-    model.val(data=r"./Lib/soda_test.yaml", epochs=200, imgsz=640, batch=32, save_json=True)
-    data_list = ['DSWD','MOCS','SODA','CHVG']
-    for data_i in data_list:
-        for type in ['small','medium','large']:
-            data_yaml = data_i+'_'+type+'.yaml'
-            print(ablition_model,data_i,type)
-            model.val(data=r"./Lib/{}".format(data_yaml),epochs=200,imgsz=640,batch=32, save_json=True)
+    model.val(data=r"./dataset/{}.yaml".format(data_type), batch=32, imgsz =size, save_json=True,  split='test',conf=0.25, iou=0.5, name = ablition_model+"/time_"+data_type+'_'+str(size) )
     print('========================================================================')
+
 
 
 
--- yolo_train.py
+++ yolo_train.py
@@ -3,14 +3,15 @@ import sys
 
 if __name__ == '__main__':
     # 加载模型
-    model = YOLO('checkpoints/yolo12s.pt')  # build a new model from scratch
-    # model_cgf = sys.argv[1]
-    # data = sys.argv[2]
-    # name = sys.argv[3]
-    print("---------------------p2_rp5_sf_yolo12s--------------------")
-    model.train(data=r"./Lib/contr_worker.yaml",
+    model_cgf = sys.argv[1]
+    data = sys.argv[2]
+    model = YOLO('yolo12s.pt')  # build a new model from scratch
+    imgsz = sys.argv[3]
+    name = model_cgf.split('.')[0]+'_'+str(imgsz )+'_'+data.split('.')[0]
+    print("-----------------------{}------------------".format(name))
+    model.train(data=r"./dataset/{}".format(data),
                 epochs=200,
-                model= "./Lib/p2_rp5_sf_yolo12s.yaml",
-                name = "sas_yolo11s",
-                imgsz=640,
-                batch=32)
+                model="./Lib/{}".format(model_cgf),
+                name = name,
+                imgsz=imgsz,
+                batch=16)
