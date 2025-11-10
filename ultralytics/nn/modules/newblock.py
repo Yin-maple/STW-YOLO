873aea08c5a59d54043e6936eeb35117ae7454e5
Yin Yufeng <1499961892@qq.com>
2025-11-10 14:28:21 +0800

    change 2

--- ultralytics/nn/modules/newblock.py
+++ ultralytics/nn/modules/newblock.py
@@ -100,39 +100,9 @@ class AAttn(nn.Module):
         return self.proj(x)
 
 class ABlock(nn.Module):
-    """
-    Area-attention block module for efficient feature extraction in YOLO models.
-
-    This module implements an area-attention mechanism combined with a feed-forward network for processing feature maps.
-    It uses a novel area-based attention approach that is more efficient than traditional self-attention while
-    maintaining effectiveness.
-
-    Attributes:
-        attn (AAttn): Area-attention module for processing spatial features.
-        mlp (nn.Sequential): Multi-layer perceptron for feature transformation.
-
-    Methods:
-        _init_weights: Initializes module weights using truncated normal distribution.
-        forward: Applies area-attention and feed-forward processing to input tensor.
-
-    Examples:
-        >>> block = ABlock(dim=256, num_heads=8, mlp_ratio=1.2, area=1)
-        >>> x = torch.randn(1, 256, 32, 32)
-        >>> output = block(x)
-        >>> print(output.shape)
-        torch.Size([1, 256, 32, 32])
-    """
 
     def __init__(self, dim, num_heads, mlp_ratio=1.2, area=1):
-        """
-        Initialize an Area-attention block module.
 
-        Args:
-            dim (int): Number of input channels.
-            num_heads (int): Number of heads into which the attention mechanism is divided.
-            mlp_ratio (float): Expansion ratio for MLP hidden dimension.
-            area (int): Number of areas the feature map is divided.
-        """
         super().__init__()
         self.attn = AAttn(dim, num_heads=num_heads, area=area)
         mlp_hidden_dim = int(dim * mlp_ratio)
@@ -141,123 +111,38 @@ class ABlock(nn.Module):
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
-        """
-        Initialize weights using a truncated normal distribution.
 
-        Args:
-            m (nn.Module): Module to initialize.
-        """
         if isinstance(m, nn.Conv2d):
             nn.init.trunc_normal_(m.weight, std=0.02)
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        """
-        Forward pass through ABlock.
 
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            (torch.Tensor): Output tensor after area-attention and feed-forward processing.
-        """
         x = x + self.attn(x)
         return x + self.mlp(x)
 
 class ABlock2(nn.Module):
-    """
-    Deformable Transformer Decoder Layer inspired by PaddleDetection and Deformable-DETR implementations.
 
-    https://github.com/PaddlePaddle/PaddleDetection/blob/develop/ppdet/modeling/transformers/deformable_transformer.py
-    https://github.com/fundamentalvision/Deformable-DETR/blob/main/models/deformable_transformer.py
-
-    Attributes:
-        self_attn (nn.MultiheadAttention): Self-attention module.
-        dropout1 (nn.Dropout): Dropout after self-attention.
-        norm1 (nn.LayerNorm): Layer normalization after self-attention.
-        cross_attn (MSDeformAttn): Cross-attention module.
-        dropout2 (nn.Dropout): Dropout after cross-attention.
-        norm2 (nn.LayerNorm): Layer normalization after cross-attention.
-        linear1 (nn.Linear): First linear layer in the feedforward network.
-        act (nn.Module): Activation function.
-        dropout3 (nn.Dropout): Dropout in the feedforward network.
-        linear2 (nn.Linear): Second linear layer in the feedforward network.
-        dropout4 (nn.Dropout): Dropout after the feedforward network.
-        norm3 (nn.LayerNorm): Layer normalization after the feedforward network.
-    """
 
     def __init__(self, d_model=256, n_heads=8, d_ffn=1024, dropout=0.0, act=nn.ReLU(), n_levels=4, n_points=4):
-        """
-        Initialize the DeformableTransformerDecoderLayer with the given parameters.
 
-        Args:
-            d_model (int): Model dimension.
-            n_heads (int): Number of attention heads.
-            d_ffn (int): Dimension of the feedforward network.
-            dropout (float): Dropout probability.
-            act (nn.Module): Activation function.
-            n_levels (int): Number of feature levels.
-            n_points (int): Number of sampling points.
-        """
         super().__init__()
 
         # Self attention
         self.aa = AAttn(d_model, n_heads)
         # attention
         self.bra = BiLevelRoutingAttention(d_model, num_heads=n_heads)
-        # self.bra = BiLevelRoutingAttention_OUR(d_model)
-
 
     def forward(self, x):
-        """
-        Perform the forward pass through the entire decoder layer.
 
-        Args:
-            x (torch.Tensor): Input embeddings.
-            attn_mask (torch.Tensor, optional): Attention mask.
-            query_pos (torch.Tensor, optional): Query position embeddings.
-
-        Returns:
-            (torch.Tensor): Output tensor after decoder layer.
-        """
         return x + self.bra(x) + self.aa(x)
 
 class ABlock3(nn.Module):
-    """
-    Deformable Transformer Decoder Layer inspired by PaddleDetection and Deformable-DETR implementations.
 
-    https://github.com/PaddlePaddle/PaddleDetection/blob/develop/ppdet/modeling/transformers/deformable_transformer.py
-    https://github.com/fundamentalvision/Deformable-DETR/blob/main/models/deformable_transformer.py
-
-    Attributes:
-        self_attn (nn.MultiheadAttention): Self-attention module.
-        dropout1 (nn.Dropout): Dropout after self-attention.
-        norm1 (nn.LayerNorm): Layer normalization after self-attention.
-        cross_attn (MSDeformAttn): Cross-attention module.
-        dropout2 (nn.Dropout): Dropout after cross-attention.
-        norm2 (nn.LayerNorm): Layer normalization after cross-attention.
-        linear1 (nn.Linear): First linear layer in the feedforward network.
-        act (nn.Module): Activation function.
-        dropout3 (nn.Dropout): Dropout in the feedforward network.
-        linear2 (nn.Linear): Second linear layer in the feedforward network.
-        dropout4 (nn.Dropout): Dropout after the feedforward network.
-        norm3 (nn.LayerNorm): Layer normalization after the feedforward network.
-    """
 
     def __init__(self, d_model=256, n_heads=8, d_ffn=1024, dropout=0.0, act=nn.ReLU(), n_levels=4, n_points=4):
-        """
-        Initialize the DeformableTransformerDecoderLayer with the given parameters.
 
-        Args:
-            d_model (int): Model dimension.
-            n_heads (int): Number of attention heads.
-            d_ffn (int): Dimension of the feedforward network.
-            dropout (float): Dropout probability.
-            act (nn.Module): Activation function.
-            n_levels (int): Number of feature levels.
-            n_points (int): Number of sampling points.
-        """
         super().__init__()
 
         # Self attention
@@ -286,23 +171,10 @@ class C3k_CSP(C3):
     """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
 
     def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
-        """
-        Initialize C3k module.
 
-        Args:
-            c1 (int): Input channels.
-            c2 (int): Output channels.
-            n (int): Number of Bottleneck blocks.
-            shortcut (bool): Whether to use shortcut connections.
-            g (int): Groups for convolutions.
-            e (float): Expansion ratio.
-            k (int): Kernel size.
-        """
         super().__init__(c1, c2, n, shortcut, g, e)
         c_ = int(c2 * e)  # hidden channels
-        # self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
         self.m = nn.Sequential(*(BottleneckCSP(c_, c_,n, shortcut, g, e=0.5) for _ in range(n)))
-        # self.m = nn.Sequential(*(SwinTransformerBlock(c_, c_, c_ // 32, n) for _ in range(n)))
 
 class C3k_ST(nn.Module):
     """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
@@ -326,79 +198,11 @@ class C3k_ST(nn.Module):
         y.extend(m(y[-1]) for m in self.m)
         y = self.cv2(torch.cat(y, 1))
         return y
-    # def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
-    #     """
-    #     Initialize CSP Bottleneck.
-    #
-    #     Args:
-    #         c1 (int): Input channels.
-    #         c2 (int): Output channels.
-    #         n (int): Number of Bottleneck blocks.
-    #         shortcut (bool): Whether to use shortcut connections.
-    #         g (int): Groups for convolutions.
-    #         e (float): Expansion ratio.
-    #     """
-    #     super().__init__()
-    #     c_ = int(c2 * e)  # hidden channels
-    #     self.cv1 = Conv(c1, c_, 1, 1)
-    #     self.cv2 = Conv((1 + n) * c_, c_, 1)
-    #     self.cv2_1 = nn.Conv2d(c1, c_, 1, 1, bias=False)
-    #     self.cv3 = nn.Conv2d(c_, c_, 1, 1, bias=False)
-    #     self.cv4 = Conv(3 * c_, c2, 1, 1)
-    #     self.bn = nn.BatchNorm2d(3 * c_)  # applied to cat(cv2, cv3)
-    #     self.act = nn.SiLU()
-    #     self.m = nn.ModuleList(SwinTransformerBlock(c_, c_, c_ // 32, n) for _ in range(n))
-    #     self.m2 = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
-    #
-    # def forward(self, x):
-    #     """Apply CSP bottleneck with 3 convolutions."""
-    #     y = [self.cv1(x)]
-    #     y.extend(m(y[-1]) for m in self.m)
-    #     y = self.cv2(torch.cat(y, 1))
-    #     y1 = self.cv3(self.m2(self.cv1(x)))
-    #     y2 = self.cv2_1(x)
-    #     return self.cv4(self.act(self.bn(torch.cat((y,y1, y2), 1))))
 
 class SFC2f(nn.Module):
-    """
-    Area-Attention C2f module for enhanced feature extraction with area-based attention mechanisms.
-
-    This module extends the C2f architecture by incorporating area-attention and ABlock layers for improved feature
-    processing. It supports both area-attention and standard convolution modes.
-
-    Attributes:
-        cv1 (Conv): Initial 1x1 convolution layer that reduces input channels to hidden channels.
-        cv2 (Conv): Final 1x1 convolution layer that processes concatenated features.
-        gamma (nn.Parameter | None): Learnable parameter for residual scaling when using area attention.
-        m (nn.ModuleList): List of either ABlock or C3k modules for feature processing.
-
-    Methods:
-        forward: Processes input through area-attention or standard convolution pathway.
-
-    Examples:
-        >>> m = SFC2f(512, 512, n=1, a2=True, area=1)
-        >>> x = torch.randn(1, 512, 32, 32)
-        >>> output = m(x)
-        >>> print(output.shape)
-        torch.Size([1, 512, 32, 32])
-    """
 
     def __init__(self, c1, c2, n=1, sf=True, e=0.5, g=1, residual=False, shortcut=True):
-        """
-        Initialize Area-Attention C2f module.
 
-        Args:
-            c1 (int): Number of input channels.
-            c2 (int): Number of output channels.
-            n (int): Number of ABlock or C3k modules to stack.
-            sf (bool): Whether to use area attention blocks. If False, uses C3k blocks instead.
-            area (int): Number of areas the feature map is divided.
-            residual (bool): Whether to use residual connections with learnable gamma parameter.
-            mlp_ratio (float): Expansion ratio for MLP hidden dimension.
-            e (float): Channel expansion ratio for hidden channels.
-            g (int): Number of groups for grouped convolutions.
-            shortcut (bool): Whether to use shortcut connections in C3k blocks.
-        """
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         assert c_ % 32 == 0, "Dimension of ABlock be a multiple of 32."
@@ -415,23 +219,10 @@ class SFC2f(nn.Module):
             else C3k_CSP(c_, c_, 2, shortcut, g)
             for _ in range(n)
         )
-        # self.m = nn.ModuleList(
-        #     nn.Sequential(*(Bottleneck_CloAtt(c_, c_, shortcut, g, k=(3, 3), e=0.5) for _ in range(1)))
-        #     if sf
-        #     else C3k(c_, c_, 2, shortcut, g)
-        #     for _ in range(n)
-        # )
+
 
     def forward(self, x):
-        """
-        Forward pass through A2C2f layer.
 
-        Args:
-            x (torch.Tensor): Input tensor.
-
-        Returns:
-            (torch.Tensor): Output tensor after processing.
-        """
         y = [self.cv1(x)]
         y.extend(m(y[-1]) for m in self.m)
         y = self.cv2(torch.cat(y, 1))
@@ -441,45 +232,9 @@ class SFC2f(nn.Module):
         return y
 
 class SFC2f3(nn.Module):
-        """
-        Area-Attention C2f module for enhanced feature extraction with area-based attention mechanisms.
-
-        This module extends the C2f architecture by incorporating area-attention and ABlock layers for improved feature
-        processing. It supports both area-attention and standard convolution modes.
-
-        Attributes:
-            cv1 (Conv): Initial 1x1 convolution layer that reduces input channels to hidden channels.
-            cv2 (Conv): Final 1x1 convolution layer that processes concatenated features.
-            gamma (nn.Parameter | None): Learnable parameter for residual scaling when using area attention.
-            m (nn.ModuleList): List of either ABlock or C3k modules for feature processing.
-
-        Methods:
-            forward: Processes input through area-attention or standard convolution pathway.
-
-        Examples:
-            >>> m = SFC2f3(512, 512, n=1, a2=True, area=1)
-            >>> x = torch.randn(1, 512, 32, 32)
-            >>> output = m(x)
-            >>> print(output.shape)
-            torch.Size([1, 512, 32, 32])
-        """
 
         def __init__(self, c1, c2, n=1, sf=True, e=0.5, g=1, residual=False, shortcut=True):
-            """
-            Initialize Area-Attention C2f module.
 
-            Args:
-                c1 (int): Number of input channels.
-                c2 (int): Number of output channels.
-                n (int): Number of ABlock or C3k modules to stack.
-                sf (bool): Whether to use area attention blocks. If False, uses C3k blocks instead.
-                area (int): Number of areas the feature map is divided.
-                residual (bool): Whether to use residual connections with learnable gamma parameter.
-                mlp_ratio (float): Expansion ratio for MLP hidden dimension.
-                e (float): Channel expansion ratio for hidden channels.
-                g (int): Number of groups for grouped convolutions.
-                shortcut (bool): Whether to use shortcut connections in C3k blocks.
-            """
             super().__init__()
             c_ = int(c2 * e)  # hidden channels
             assert c_ % 32 == 0, "Dimension of ABlock be a multiple of 32."
@@ -496,23 +251,10 @@ class SFC2f3(nn.Module):
                 else C3k_CSP(c_, c_, 2, shortcut, g)
                 for _ in range(n)
             )
-            # self.m = nn.ModuleList(
-            #     nn.Sequential(*(Bottleneck_CloAtt(c_, c_, shortcut, g, k=(3, 3), e=0.5) for _ in range(1)))
-            #     if sf
-            #     else C3k(c_, c_, 2, shortcut, g)
-            #     for _ in range(n)
-            # )
+
 
         def forward(self, x):
-            """
-            Forward pass through A2C2f layer.
 
-            Args:
-                x (torch.Tensor): Input tensor.
-
-            Returns:
-                (torch.Tensor): Output tensor after processing.
-            """
             y = [self.cv1(x)]
             y.extend(m(y[-1]) for m in self.m)
             y = self.cv2(torch.cat(y, 1))
